86edb9494a28ac3b7aadf713c323f8d3907ad7f2
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2019-01-25 11:07:32 +0100

    update README, update route.rb

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 * run `rails db:migrate` then run `rails server`
 
 * The app is served at `localhost:3000`
+
+
+* The paths available are ` gets: '/new', '/all', '/user/:id/edit'. post: '/users/create'`
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create,:edit,:update, :index]
-  root 'users#index'
-  # get '/new', to: 'users#new'
-  # get '/user/:id/edit', to: 'users#edit'
-  # post '/users/create', to: 'users#create' 
+  resources :users
+  root 'users#new'
+  get '/new', to: 'users#new'
+  get '/all', to: 'users#index'
+  get '/user/:id/edit', to: 'users#edit'
+  post '/users/create', to: 'users#create' 
 end
